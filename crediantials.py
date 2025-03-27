df957fce9158cf80e01131d5dd0e2b5904323f88
niharika17032001 <niharika17032001@gmail.com>
2025-03-27 20:19:49 +0530

    Create blank.yml

--- crediantials.py
+++ crediantials.py
@@ -6,6 +6,13 @@ load_dotenv()
 USER = os.getenv("USER")
 PWD = os.getenv("YOUTUBE_PWD")
 
+file_path = os.path.abspath(__file__)
+current_Folder_Path = os.path.dirname(file_path)
+root_folder = os.path.dirname(current_Folder_Path)
+
+print(f"Current Folder Path: {current_Folder_Path}")
+print(f"Root Folder Path: {root_folder}")
+screenshot_path = current_Folder_Path + '/reports/screenshot.png'
 
 if not PWD:
     raise ValueError("PWD key is missing. Ensure it is set in GitHub Secrets.")
